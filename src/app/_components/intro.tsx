8e4d049e215daeab7920eb18cc3b24e798152245
Eric allen <eric@ericrallen.dev>
2024-03-03 12:06:32 -0500

    fix: remove old constants

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,5 +1,3 @@
-import { CMS_NAME } from "@/lib/constants";
-
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
